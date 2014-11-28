04068c0846c248c50c0a2988c20a11472ea1de7f
Raphael Haddad <raphael@haddad.net.au>
2014-11-28 13:11:26 +1100

    adding new lines

--- Newtonsoft.Json.Serialization.ContractResolverExtentions/SnakeCasePropertyNamesContractResolver.cs
+++ Newtonsoft.Json.Serialization.ContractResolverExtentions/SnakeCasePropertyNamesContractResolver.cs
@@ -5,7 +5,8 @@
         protected override string ResolvePropertyName(string propertyName)
         {
             var startUnderscores = System.Text.RegularExpressions.Regex.Match(propertyName, @"^_+");
-            return startUnderscores + System.Text.RegularExpressions.Regex.Replace(propertyName, @"([A-Z])", "_$1").ToLower().TrimStart('_');
+            return startUnderscores + System.Text.RegularExpressions.Regex
+                .Replace(propertyName, @"([A-Z])", "_$1").ToLower().TrimStart('_');
         }
     }
 }
