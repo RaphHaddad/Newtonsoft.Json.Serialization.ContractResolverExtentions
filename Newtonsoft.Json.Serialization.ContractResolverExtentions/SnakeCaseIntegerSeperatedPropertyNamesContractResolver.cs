99f6d8703eb2162df7a1514711c9c295f41a0bc5
Raphael Haddad <raphael@haddad.net.au>
2014-11-28 11:36:37 +1100

    initial commit

---
+++ Newtonsoft.Json.Serialization.ContractResolverExtentions/SnakeCaseIntegerSeperatedPropertyNamesContractResolver.cs
@@ -0,0 +1,11 @@
+﻿namespace Newtonsoft.Json.Serialization.ContractResolverExtentions
+{
+    public class SnakeCaseIntegerSeperatedPropertyNamesContractResolver : DefaultContractResolver
+    {
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            var startUnderscores = System.Text.RegularExpressions.Regex.Match(propertyName, @"^_+");
+            return startUnderscores + System.Text.RegularExpressions.Regex.Replace(propertyName, @"([A-Z0-9])", "_$1").ToLower().TrimStart('_');
+        }
+    }	
+}
---
+++ Newtonsoft.Json.Serialization.ContractResolverExtentions/SnakeCasePropertyNamesContractResolver.cs
@@ -0,0 +1,11 @@
+﻿namespace Newtonsoft.Json.Serialization.ContractResolverExtentions
+{
+    public class SnakeCasePropertyNamesContractResolver : DefaultContractResolver
+    {
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            var startUnderscores = System.Text.RegularExpressions.Regex.Match(propertyName, @"^_+");
+            return startUnderscores + System.Text.RegularExpressions.Regex.Replace(propertyName, @"([A-Z])", "_$1").ToLower().TrimStart('_');
+        }
+    }
+}
